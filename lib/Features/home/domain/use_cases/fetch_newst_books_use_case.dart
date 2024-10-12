f754f317c36c191acb52bb1d88374dd8d849b15c
ahmed amer <01099517574med@gmail.com>
2024-10-12 19:50:15 +0300

    fetch newest books use case

---
+++ lib/Features/home/domain/use_cases/fetch_newst_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:bookcleanarch_app/Features/home/domain/entites/book_entity.dart';
+import 'package:bookcleanarch_app/Features/home/domain/repos/home_repo.dart';
+import 'package:bookcleanarch_app/core/errors/failure.dart';
+import 'package:bookcleanarch_app/core/use_cases/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCse extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
+
+  FetchNewestBooksUseCse(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+    return await homeRepo.fetchNewstBooks();
+  }
+}
