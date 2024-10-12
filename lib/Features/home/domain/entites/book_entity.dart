52e8052ed4113727960550ee7d1d863887b27905
ahmed amer <01099517574med@gmail.com>
2024-10-12 14:11:14 +0300

    a7a

---
+++ lib/Features/home/domain/entites/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authrName;
+  final num price;
+  final num tating;
+
+  BookEntity(this.image, this.title, this.authrName, this.price, this.tating);
+}
