17167190763b84a8342c4f15769153a57826c07b
ahmed amer <01099517574med@gmail.com>
2024-10-12 14:48:11 +0300

    add either class

--- lib/Features/home/domain/repos/home_repo.dart
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
 import 'package:bookcleanarch_app/Features/home/domain/entites/book_entity.dart';
+import 'package:bookcleanarch_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
-    Future<List<BookEntity>>fetchFeaturedBooks();
-    Future<List<BookEntity>>fetchNewstBooks();
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewstBooks();
 }
