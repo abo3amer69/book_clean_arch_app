721603f95cfc4e3154acbfaf91bf9de0fa271b61
ahmed amer <01099517574med@gmail.com>
2024-10-14 19:18:19 +0300

    register ox and open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:bookcleanarch_app/Features/home/domain/entites/book_entity.dart';
 import 'package:bookcleanarch_app/constants.dart';
 import 'package:bookcleanarch_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 import 'Features/Splash/presentation/views/splash_view.dart';
 
-void main() {
-  runApp(const Bookly());
+void main() async {
+  runApp(
+    const Bookly(),
+  );
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(KFeaturedbox);
 }
 
 class Bookly extends StatelessWidget {
